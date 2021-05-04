a28291807522c3765b866be8ad6474a139be0ef9
Windmill-City <1449182174@qq.com>
2021-05-04 21:35:23 +0800

    fix serverPort support non-exist file

--- main.js
+++ main.js
@@ -7,10 +7,7 @@ function getBody(bodyOrFile, convertMarkdown) {
     let body = bodyOrFile
 
     // Read body from file
-    if (bodyOrFile.startsWith("file://")) {
-        const file = bodyOrFile.replace("file://", "")
-        body = fs.readFileSync(file, "utf8")
-    }
+    body = getFromFile(bodyOrFile)
 
     // Convert Markdown to HTML
     if (convertMarkdown) {
@@ -29,31 +26,74 @@ function getFrom(from, username) {
     return `"${from}" <${username}>`
 }
 
+function getFromFile(arg) {
+    if (arg.startsWith("file://")) {
+        const file = arg.replace("file://", "")
+        try {
+            return fs.readFileSync(file, "utf8")
+        } catch (e) {
+            console.log(e)
+            return "[Error]File:" + file + " non-exist!"
+        }
+    }
+    return arg
+}
+
 async function main() {
     try {
-        const serverAddress = core.getInput("server_address", { required: false })
-        const serverPort = core.getInput("server_port", { required: false })
-        const username = core.getInput("username", { required: true })
-        const password = core.getInput("password", { required: true })
-        const subject = core.getInput("subject", { required: true })
-        const from = core.getInput("from", { required: true })
-        const to = core.getInput("to", { required: true })
-        const secure = core.getInput("secure", { required: false })
-        const body = core.getInput("body", { required: false })
-        const htmlBody = core.getInput("html_body", { required: false })
-        const cc = core.getInput("cc", { required: false })
-        const bcc = core.getInput("bcc", { required: false })
-        const replyTo = core.getInput("reply_to", { required: false })
-        const attachments = core.getInput("attachments", { required: false })
-        const convertMarkdown = core.getInput("convert_markdown", { required: false })
-        const ignoreCert = core.getInput("ignore_cert", { required: false })
+        const serverAddress = core.getInput("server_address", {
+            required: false
+        })
+        const serverPort = core.getInput("server_port", {
+            required: false
+        })
+        const username = core.getInput("username", {
+            required: true
+        })
+        const password = core.getInput("password", {
+            required: true
+        })
+        const subject = core.getInput("subject", {
+            required: true
+        })
+        const from = core.getInput("from", {
+            required: true
+        })
+        const to = core.getInput("to", {
+            required: true
+        })
+        const secure = core.getInput("secure", {
+            required: false
+        })
+        const body = core.getInput("body", {
+            required: false
+        })
+        const htmlBody = core.getInput("html_body", {
+            required: false
+        })
+        const cc = core.getInput("cc", {
+            required: false
+        })
+        const bcc = core.getInput("bcc", {
+            required: false
+        })
+        const replyTo = core.getInput("reply_to", {
+            required: false
+        })
+        const attachments = core.getInput("attachments", {
+            required: false
+        })
+        const convertMarkdown = core.getInput("convert_markdown", {
+            required: false
+        })
+        const ignoreCert = core.getInput("ignore_cert", {
+            required: false
+        })
 
-        mailRegEx = /(\w+)@(\w+\.\w+\.\w+)?/
-        mailRegEx.test(username)
         const transport = nodemailer.createTransport({
-            host: serverAddress ? serverAddress : "smtp." + RegExp.$2,
-            port: serverPort ? server_port : "465",
-            secure: secure ? true : serverPort == "465",
+            host: serverAddress ? serverAddress : "smtp." + username.substring(username.indexOf("@") + 1),
+            port: serverPort ? serverPort : 465,
+            secure: secure || !serverPort ? true : serverPort == 465,
             auth: {
                 user: username,
                 pass: password,
@@ -63,23 +103,21 @@ async function main() {
             } : undefined
         })
 
-        if (subject.startsWith("file://")){
-            const file = subject.replace("file://", "")
-            subject = fs.readFileSync(file, "utf8")
-        }
-
         const info = await transport.sendMail({
             from: getFrom(from, username),
             to: to,
-            subject: subject,
+            subject: getFromFile(subject),
             cc: cc ? cc : undefined,
             bcc: bcc ? bcc : undefined,
             replyTo: replyTo ? replyTo : undefined,
             text: body ? getBody(body, false) : undefined,
             html: htmlBody ? getBody(htmlBody, convertMarkdown) : undefined,
-            attachments: attachments ? attachments.split(',').map(f => ({ path: f.trim() })) : undefined
+            attachments: attachments ? attachments.split(',').map(f => ({
+                path: f.trim()
+            })) : undefined
         })
     } catch (error) {
+        console.log(error)
         core.setFailed(error.message)
     }
 }
